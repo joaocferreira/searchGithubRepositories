618ed71d4e7ace7e2b20ebdd1939124a85432a62
nomad0 <f16123@gmail.com>
2014-11-06 21:49:24 +0200

    2

--- js/app.js
+++ js/app.js
@@ -2,12 +2,27 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('SearchListController', function($scope, $http){
 
-$scope.makeQuery = function(){
+$scope.Math = window.Math;
+
+$scope.makeQuery = function(nPage){
+	
+	var app = this;
+	$scope.actualPage = nPage;
+	$scope.itemsPerPage = 10;
 $http.get('https://api.github.com/search/repositories', 
-{ params: { q: $scope.searchName, sort: $scope.searchSort}})
+{ params: { order: $scope.searchOrder, q: $scope.searchName, sort: $scope.sortBy, per_page: $scope.itemsPerPage, page: $scope.actualPage}})
 .success(function(data) {
-  $scope.results = data; 
+  app.results = data; 
   }); 
+};	
+$scope.movePageBack = function(){	
+	$scope.actualPage =  $scope.actualPage - 1;
+	$scope.makeQuery($scope.actualPage);
+};
+
+$scope.movePageForward = function(){
+	$scope.actualPage =  $scope.actualPage + 1;
+	$scope.makeQuery($scope.actualPage);
 };
 
 });
